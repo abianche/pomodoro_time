01c8aaf73530d0d62a3ace88b3c2ffdcbab540b3
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2020-01-22 23:28:32 +0100

    check for timer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pomodoro_time/models/pomodoro.dart' show PomodoroState;
 import 'package:pomodoro_time/pomodoro.dart';
+import 'package:pomodoro_time/redux/store.dart';
 import 'package:pomodoro_time/routes.dart';
 
 class HomePage extends StatelessWidget {
@@ -12,10 +14,45 @@ class HomePage extends StatelessWidget {
         title: Text("Pomodoro Time"),
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.settings),
-            onPressed: () =>
-                Navigator.of(context).pushNamed(AppRoutes.settings),
-          )
+              icon: Icon(Icons.settings),
+              onPressed: () async {
+                if (appStore.state.pomodoro.state == PomodoroState.none) {
+                  Navigator.of(context).pushNamed(AppRoutes.settings);
+                  return;
+                }
+
+                bool confirmed = await showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text("title"),
+                        content: Text("asd"),
+                        actions: [
+                          FlatButton(
+                            child: Text(MaterialLocalizations.of(context)
+                                .cancelButtonLabel),
+                            onPressed: () {
+                              Navigator.of(context).pop(false);
+                            },
+                          ),
+                          FlatButton(
+                            child: Text(MaterialLocalizations.of(context)
+                                .continueButtonLabel),
+                            onPressed: () {
+                              Navigator.of(context).pop(true);
+                            },
+                          ),
+                        ],
+                      );
+                    });
+
+                if (confirmed == null || !confirmed) {
+                  return;
+                }
+
+                /// TODO: stop timer
+                Navigator.of(context).pushNamed(AppRoutes.settings);
+              })
         ],
       ),
       body: Center(
